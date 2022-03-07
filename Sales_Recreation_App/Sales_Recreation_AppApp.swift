1206eb71cd7732ca27aef59801fb91f381acd260
Adam Januszewski <Adpjan@att.net>
2022-03-07 09:34:49 -0500

    Initial Commit

---
+++ Sales_Recreation_App/Sales_Recreation_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sales_Recreation_AppApp.swift
+//  Sales_Recreation_App
+//
+//  Created by adam janusewski on 3/7/22.
+//
+
+import SwiftUI
+
+@main
+struct Sales_Recreation_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Sales_Recreation_AppUITests/Sales_Recreation_AppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Sales_Recreation_AppUITestsLaunchTests.swift
+//  Sales_Recreation_AppUITests
+//
+//  Created by adam janusewski on 3/7/22.
+//
+
+import XCTest
+
+class Sales_Recreation_AppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
